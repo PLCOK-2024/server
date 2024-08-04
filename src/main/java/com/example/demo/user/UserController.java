7b6f9d66f200f1a56c0bb9435f4a70c5ea507839
samkimpepper <62634206+samkimpepper@users.noreply.github.com>
2024-08-04 10:41:44 +0900

    feat: swagger 설정

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -2,23 +2,28 @@ package com.example.demo.user;
 
 import com.example.demo.user.dto.SignupRequest;
 import com.example.demo.user.dto.UserDetailResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Tag(name = "회원 API")
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "회원가입")
     @PostMapping
     public ResponseEntity<Long> signUp(@RequestBody SignupRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(userService.signUp(request));
     }
 
+    @Operation(summary = "회원 상세 조회")
     @GetMapping("/{userId}")
     public ResponseEntity<UserDetailResponse> getUserDetail(@PathVariable Long userId) {
         return ResponseEntity.ok(userService.getUserDetail(userId));
