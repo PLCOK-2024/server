e49db5fc41f1bd12c1b53222fd7c00b967e32000
keeeeeey <sseioul@naver.com>
2024-08-13 20:47:48 +0900

    feat: 소셜 로그인

--- src/main/java/com/example/demo/user/controller/AuthUserController.java
+++ src/main/java/com/example/demo/user/controller/AuthUserController.java
@@ -1,16 +1,14 @@
 package com.example.demo.user.controller;
 
 import com.example.demo.user.dto.SocialLoginRequest;
-import com.example.demo.user.dto.TokenResponse;
+import com.example.demo.user.dto.SocialLoginResponse;
 import com.example.demo.user.service.AuthUserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,8 +19,22 @@ public class AuthUserController {
     private final AuthUserService authUserService;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(@RequestBody SocialLoginRequest requestDto) {
+    public ResponseEntity<SocialLoginResponse> login(@RequestBody SocialLoginRequest requestDto) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(authUserService.login(requestDto));
+    }
+
+    @GetMapping("/user/kakao/callback")
+    public ResponseEntity<SocialLoginResponse> kakaoLogin(@RequestParam("code") String code) throws JsonProcessingException {
+        String accessToken = authUserService.getToken(code, "Kakao");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(authUserService.login(new SocialLoginRequest(accessToken, "Kakao")));
+    }
+
+    @GetMapping("/user/google/callback")
+    public ResponseEntity<SocialLoginResponse> googleLogin(@RequestParam("code") String code) throws JsonProcessingException {
+        String accessToken = authUserService.getToken(code, "Google");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(authUserService.login(new SocialLoginRequest(accessToken, "Google")));
     }
 }
--- src/main/java/com/example/demo/user/domain/UserProvider.java
+++ src/main/java/com/example/demo/user/domain/UserProvider.java
@@ -4,12 +4,14 @@ import com.example.demo.common.BaseEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "user_providers", schema = "test")
 @AttributeOverrides({
         @AttributeOverride(name = "createdAt", column = @Column(name = "created_at", nullable = false))
@@ -34,4 +36,12 @@ public class UserProvider extends BaseEntity {
     @NotNull
     @Column(name = "provider_user_id", nullable = false, length = 100)
     private String providerUserId;
+
+    public static UserProvider of(String providerType, User user, String providerUserId) {
+        return builder()
+                .providerType(providerType)
+                .user(user)
+                .providerUserId(providerUserId)
+                .build();
+    }
 }
